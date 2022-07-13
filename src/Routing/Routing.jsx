c7bbfd969b851a4d1bc47c7360d43749f53b0f25
chaze77 <cdaniyar@gmail.com>
2022-07-13 17:43:19 +0600

    added main

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Intro from "../components/Intro/Intro";
+import Main from "../components/Main/Main";
 
 const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<Intro />} />
+      {/* <Route path="/" element={<Main />} /> */}
     </Routes>
   );
 };
