92006608b879e9f27f15159f4f71c53a9f68452a
baiysh1218 <baiyshbeksultanov1@gmail.com>
2022-07-19 14:24:50 +0600

    aded jnij

--- src/components/MainFooter/MainFooter.jsx
+++ src/components/MainFooter/MainFooter.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import "../MainFooter/MainFooter.css";
 
 const MainFooter = () => {
   return (
-    <div className="footer-intro-content">
-      <div className="footer-content">
-        <h3 className="footer-intro-h3">Одежда</h3>
-      </div>
-      <div>
-        <p className="footer-content-p">Accessories Edition</p>
-        <p className="footer-content-p">White Summer</p>
-        <p className="footer-content-p">Denim Shorts Guide</p>
-        <p className="footer-content-p">Платья и комбинезоны</p>
-        <p className="footer-content-p">Рубашки</p>
-        <p className="footer-content-p">Обувь</p>
+    <div className="container">
+      <div className="footer-intro-content">
+        <div className="footer-content">
+          <h3 className="footer-intro-h3">Одежда</h3>
+        </div>
+        <div>
+          <p className="footer-content-p">Accessories Edition</p>
+          <p className="footer-content-p">White Summer</p>
+          <p className="footer-content-p">Denim Shorts Guide</p>
+          <p className="footer-content-p">Платья и комбинезоны</p>
+          <p className="footer-content-p">Рубашки</p>
+          <p className="footer-content-p">Обувь</p>
+        </div>
       </div>
     </div>
   );
